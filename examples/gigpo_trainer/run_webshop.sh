4b080f743d89b5fdde80b34818ba76eba890a028
Wendi Li <windy@galaxy-05.cs.wisc.edu>
2026-05-23 18:48:48 -0500

    Update code

--- examples/gigpo_trainer/run_webshop.sh
+++ examples/gigpo_trainer/run_webshop.sh
@@ -34,6 +34,7 @@ HYDRA_FULL_ERROR=1 CUDA_VISIBLE_DEVICES=1,0 python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_kl_loss=True \
+    +actor_rollout_ref.actor.classification_loss_coef=0.02 \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
